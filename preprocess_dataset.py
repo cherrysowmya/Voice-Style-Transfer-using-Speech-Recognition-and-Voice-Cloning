1d2a415db6397991937a4a021f7b9631a1953c57
SOWMYA CHERIPALLY <sowmya.cherry@yahoo.com>
2023-06-26 17:20:24 +0530

    extra changes

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,12 +1,18 @@
-import os, shutil
+import os, shutil, argparse
 from utils.get_vocals import get_vocals
 from utils.create_dataset import create_dataset
 
-SONGS_PATH = "audio_files/songs"
-VOCALS_PATH = "audio_files/vocals_only"
-DATASET_PATH = "audio_files/dataset"
+SONGS_PATH = "./audio_files/songs"
+VOCALS_PATH = "./audio_files/vocals_only"
+DATASET_PATH = "./audio_files/dataset"
 
 if __name__ == "__main__":
-    get_vocals(SONGS_PATH, VOCALS_PATH)
-    create_dataset(VOCALS_PATH, DATASET_PATH)
-    if os.path.exists(VOCALS_PATH): shutil.rmtree(VOCALS_PATH)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--songs_path", type=str, default=SONGS_PATH, help="songs directory")
+    parser.add_argument("--vocals_path", type=str, default=VOCALS_PATH, help="vocals only directory")
+    parser.add_argument("--dataset_path", type=str, default=DATASET_PATH, help="dataset directory")
+    args = parser.parse_args()
+
+    get_vocals(args.songs_path, args.vocals_path)
+    create_dataset(args.vocals_path, args.dataset_path)
+    if os.path.exists(args.vocals_path): shutil.rmtree(args.vocals_path)
